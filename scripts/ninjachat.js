f2391f1eb2d6619012e11bcae75573eb2db54761
remotepixels <app+github@remotepixels.app>
2025-01-16 16:57:00 +0100

    trying to capture clicks to change values

--- scripts/ninjachat.js
+++ scripts/ninjachat.js
@@ -1,15 +1,28 @@
 function replacething () {
-    
-    var html = document.querySelector('html');
-    var walker = document.createTreeWalker(html, NodeFilter.SHOW_TEXT);
-    var node;
-    while (node = walker.nextNode()) {
-      node.nodeValue = node.nodeValue.replace(/Chat/, 'File downloads');
-      node.nodeValue = node.nodeValue.replace(/Do you trust them\?/, '');
-        }
-      }
-  
-  
-  //setInterval(replacething, 50);
-  
-  //var x = document.getElementById("myLI").parentElement.nodeName;
+    alert("clicked");
+  //var html = document.querySelector('html');
+  //var walker = document.createTreeWalker(html, NodeFilter.SHOW_TEXT);
+  //var node;
+  //while (node = walker.nextNode()) {
+  //  node.nodeValue = node.nodeValue.replace(/Do you trust them\?/, '');
+   //   }
+  return;
+    }
+
+//document.getElementById("chatbutton").onclick = function() {replacething()};
+//setInterval(replacething, 500);
+
+//var x = document.getElementById("myLI").parentElement.nodeName;
+
+document.body.addEventListener("click", function (evt) {
+  var html = document.querySelector('html');
+  var walker = document.createTreeWalker(html, NodeFilter.SHOW_TEXT);
+  var node;
+  while (node = walker.nextNode()) {
+    node.nodeValue = node.nodeValue.replace(/Do you trust them\?/, '');
+     }
+    console.dir(this);
+    //note evt.target can be a nested element, not the body element, resulting in misfires
+    console.log(evt.target);
+    //alert("body clicked");
+});
