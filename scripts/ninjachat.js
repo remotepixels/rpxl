e539ec5ce00e132090f3457e9e441b99c454bd4c
remotepixels <app+github@remotepixels.app>
2025-01-16 16:03:57 +0100

    dissable javascript injection, fix chat module

--- scripts/ninjachat.js
+++ scripts/ninjachat.js
@@ -1,1 +1,15 @@
-alert("Javascript loaded");
+function replacething () {
+    
+    var html = document.querySelector('html');
+    var walker = document.createTreeWalker(html, NodeFilter.SHOW_TEXT);
+    var node;
+    while (node = walker.nextNode()) {
+      node.nodeValue = node.nodeValue.replace(/Chat/, 'File downloads');
+      node.nodeValue = node.nodeValue.replace(/Do you trust them\?/, '');
+        }
+      }
+  
+  
+  //setInterval(replacething, 50);
+  
+  //var x = document.getElementById("myLI").parentElement.nodeName;
