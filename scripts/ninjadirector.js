3f42fc82ed02bef0a84ab55965c3458b745a7ab1
remotepixels <app+github@remotepixels.app>
2025-01-24 12:52:41 +0100

    try 50ms delay in script

--- scripts/ninjadirector.js
+++ scripts/ninjadirector.js
@@ -1,19 +1,32 @@
-function replacething () {
-    alert("clicked");
+//function replacething () {
+  //  alert("clicked");
   //var html = document.querySelector('html');
   //var walker = document.createTreeWalker(html, NodeFilter.SHOW_TEXT);
   //var node;
   //while (node = walker.nextNode()) {
   //  node.nodeValue = node.nodeValue.replace(/Do you trust them\?/, '');
    //   }
-  return;
-    }
+  //return;
+//    }
 
 //document.getElementById("chatbutton").onclick = function() {replacething()};
 //setInterval(replacething, 500);
 
 //var x = document.getElementById("myLI").parentElement.nodeName;
 
+setTimeout(
+  function() {
+    var html = document.querySelector('html');
+    var walker = document.createTreeWalker(html, NodeFilter.SHOW_TEXT);
+    var node;
+    while (node = walker.nextNode()) {
+      node.nodeValue = node.nodeValue.replace(/enable director`s microphone or video/, 'Start main stream');
+    }
+  }, 50);
+
+
+
+/*
 document.body.addEventListener("click", function (evt) {
   var html = document.querySelector('html');
   var walker = document.createTreeWalker(html, NodeFilter.SHOW_TEXT);
@@ -21,8 +34,8 @@ document.body.addEventListener("click", function (evt) {
   while (node = walker.nextNode()) {
     node.nodeValue = node.nodeValue.replace(/enable director`s microphone or video/, 'Start main stream');
      }
-    console.dir(this);
+   // console.dir(this);
     //note evt.target can be a nested element, not the body element, resulting in misfires
-    console.log(evt.target);
+   // console.log(evt.target);
     //alert("body clicked");
-});
+});*/
